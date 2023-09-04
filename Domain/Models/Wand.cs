f12a58de411f53e2b5a84b9915a1d08199904138
Maks0s <mrmaksman01@mail.ru>
2023-09-04 16:23:33 +0300

    Add Model: Wand

---
+++ Domain/Models/Wand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Wand
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Core { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Wood { get; set; }
+        [Required]
+        [Range(6, 17)]
+        [Column(TypeName = "decimal(4,2)")]
+        public decimal LengthInInches { get; set; }
+        [MaxLength(80)]
+        public string? TrueOwner { get; set; }
+    }
+}
