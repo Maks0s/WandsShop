528a973fef190da1e7fd63d4508dcbbbcc7170ab
Maks0s <mrmaksman01@mail.ru>
2023-09-04 17:10:51 +0300

    Add Interface of WandRepository

---
+++ Application/Common/Interfaces/Persistence/IWandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.Persistence
+{
+    public interface IWandRepository
+    {
+        Wand CreateWand(Wand wand);
+        Wand GetWandById(int id);
+        ICollection<Wand> GetAllWands();
+        void UpdateWand(Wand wand);
+        void DeleteWand(int id);
+    }
+}
