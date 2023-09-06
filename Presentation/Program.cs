ed681caeb6a1baca597915ad62b8bee74d7cdd39
Maks0s <mrmaksman01@mail.ru>
2023-09-06 11:45:37 +0300

    Add base global logging

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,31 +1,62 @@
 using Infrastructure;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddControllers();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .CreateBootstrapLogger();
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Information("Started app building");
+
+    builder.Host.UseSerilog((_, loggerConfig) =>
+    {
+        var jsonConfig = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", 
+                true, reloadOnChange: true)
+            .Build();
+
+        loggerConfig
+            .ReadFrom.Configuration(jsonConfig);
+    });
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+	builder.Services.AddControllers();
+
+
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Error(ex, "Crushed during the building");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
