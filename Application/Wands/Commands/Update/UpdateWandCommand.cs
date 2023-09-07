3f25a883feac927c8b01f0148736f0f658d9efbb
Maks0s <mrmaksman01@mail.ru>
2023-09-07 12:26:43 +0300

    Add UpdateWandCommand and Handler

---
+++ Application/Wands/Commands/Update/UpdateWandCommand.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.CQRS;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wands.Commands.Update
+{
+    public record UpdateWandCommand(
+        int Id,
+        string? Core,
+        string? Wood,
+        decimal? LengthInInches,
+        string? Owner,
+        string? Description
+        ) : ICommand<ErrorOr.Updated>;
+}
---
+++ Application/Wands/Commands/Update/UpdateWandCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using Application.Common.CQRS;
+using Application.Common.Interfaces.Persistence;
+using Domain.Models;
+using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wands.Commands.Update
+{
+    public class UpdateWandCommandHandler : ICommandHandler<UpdateWandCommand, ErrorOr.Updated>
+    {
+        private readonly IWandRepository _wandRepository;
+
+        public UpdateWandCommandHandler(IWandRepository wandRepository)
+        {
+            _wandRepository = wandRepository;
+        }
+
+        public async Task<ErrorOr<Updated>> Handle(UpdateWandCommand command, CancellationToken cancellationToken)
+        {
+            var wandToUpdate = await _wandRepository.GetWandByIdAsync(command.Id);
+
+            MapToWandToUpdate(wandToUpdate, command);
+
+            await _wandRepository.UpdateWandAsync(wandToUpdate);
+
+            return Result.Updated;
+        }
+
+        private void MapToWandToUpdate(Wand wandToUpdate, UpdateWandCommand command)
+        {
+            if(command.Core  != null)
+            {
+                wandToUpdate.Core = command.Core;
+            }
+
+            if (command.Wood != null)
+            {
+                wandToUpdate.Wood = command.Wood;
+            }
+
+            if (command.LengthInInches != null)
+            {
+                wandToUpdate.LengthInInches = (decimal)command.LengthInInches;
+            }
+
+            if (command.Owner != null)
+            {
+                wandToUpdate.Owner = command.Owner;
+            }
+
+            if (command.Description != null)
+            {
+                wandToUpdate.Description = command.Description;
+            }
+        }
+    }
+}
