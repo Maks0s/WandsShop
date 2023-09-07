192abf27b2ab29b1d5e1f6d2e6c1d2e13c708334
Maks0s <mrmaksman01@mail.ru>
2023-09-07 12:30:25 +0300

    Add DeleteWandCommand and Handler

---
+++ Application/Wands/Commands/Delete/DeleteWandCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wands.Commands.Delete
+{
+    public record DeleteWandCommand(int Id) : ICommand<ErrorOr.Deleted>;
+}
---
+++ Application/Wands/Commands/Delete/DeleteWandCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.CQRS;
+using Application.Common.Interfaces.Persistence;
+using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wands.Commands.Delete
+{
+    internal class DeleteWandCommandHandler : ICommandHandler<DeleteWandCommand, ErrorOr.Deleted>
+    {
+        private readonly IWandRepository _wandRepository;
+
+        public DeleteWandCommandHandler(IWandRepository wandRepository)
+        {
+            _wandRepository = wandRepository;
+        }
+
+        public async Task<ErrorOr<Deleted>> Handle(DeleteWandCommand command, CancellationToken cancellationToken)
+        {
+            await _wandRepository.DeleteWandAsync(command.Id);
+
+            return Result.Deleted;
+        }
+    }
+}
