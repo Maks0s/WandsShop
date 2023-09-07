7861b384eff0fb81f43e4b3ea7ea8d636e899e91
Maks0s <mrmaksman01@mail.ru>
2023-09-07 12:26:28 +0300

    Add CreateWandCommand and Handler

---
+++ Application/Wands/Commands/Create/CreateWandCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.CQRS;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wands.Commands.Create
+{
+    public record CreateWandCommand(
+        string Core,
+        string Wood,
+        decimal LengthInInches,
+        string? Owner,
+        string Description
+        ) : ICommand<Wand>;
+}
---
+++ Application/Wands/Commands/Create/CreateWandCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.CQRS;
+using Application.Common.Interfaces.Persistence;
+using Domain.Models;
+using ErrorOr;
+
+namespace Application.Wands.Commands.Create
+{
+    public class CreateWandCommandHandler
+        : ICommandHandler<CreateWandCommand, Wand>
+    {
+        private readonly IWandRepository _wandRepository;
+
+        public CreateWandCommandHandler(IWandRepository wandRepository)
+        {
+            _wandRepository = wandRepository;
+        }
+
+        public async Task<ErrorOr<Wand>> Handle(CreateWandCommand command, CancellationToken cancellationToken)
+        {
+            var wandToCreate = new Wand()
+            {
+                Core = command.Core,
+                Wood = command.Wood,
+                LengthInInches = command.LengthInInches,
+                Owner = command.Owner,
+                Description = command.Description
+            };
+
+            await _wandRepository.CreateWandAsync(wandToCreate);
+
+            return wandToCreate;
+        }
+    }
+}
