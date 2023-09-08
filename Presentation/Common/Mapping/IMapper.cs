06ffc76dfd7b76a6a64db587337275430624a572
Maks0s <mrmaksman01@mail.ru>
2023-09-08 11:02:20 +0300

    Add Mepperly as a mapper and implement mapping logic

---
+++ Presentation/Common/Mapping/IMapper.cs
@@ -0,0 +1,17 @@
+﻿using Application.Wands.Commands.Create;
+using Application.Wands.Commands.Update;
+using Domain.Models;
+using Presentation.Common.DTO.Request;
+using Presentation.Common.DTO.Response;
+
+namespace Presentation.Common.Mapping
+{
+    public interface IMapper
+    {
+        public CreateWandCommand MapToCreateWandCommand(CreateWandRequest createWandRequest);
+        public UpdateWandCommand MapToUpdateWandCommand(UpdateWandRequest updateWandRequest);
+
+        public WandResponse MapToWandResponse(Wand wand);
+        public ICollection<WandResponse> MapToCollectionOfWandResponses(ICollection<Wand> wands);
+    }
+}
---
+++ Presentation/Common/Mapping/Mapper.cs
@@ -0,0 +1,28 @@
+﻿using Application.Wands.Commands.Create;
+using Application.Wands.Commands.Update;
+using Domain.Models;
+using Presentation.Common.DTO.Request;
+using Presentation.Common.DTO.Response;
+using Riok.Mapperly.Abstractions;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+
+namespace Presentation.Common.Mapping
+{
+    [Mapper]
+    public partial class Mapper : IMapper
+    {
+        public partial CreateWandCommand MapToCreateWandCommand(CreateWandRequest createWandRequest);
+        public partial UpdateWandCommand MapToUpdateWandCommand(UpdateWandRequest updateWandRequest);
+
+        public partial WandResponse MapToWandResponse(Wand wand);
+        public ICollection<WandResponse> MapToCollectionOfWandResponses(ICollection<Wand> wands)
+        {
+            var wandResponseCollection = new ConcurrentQueue<WandResponse>();
+            Parallel.ForEach<Wand>(wands, (wand) => 
+                wandResponseCollection.Enqueue(MapToWandResponse(wand)));
+
+            return wandResponseCollection.ToImmutableList();
+        }
+    }
+}
