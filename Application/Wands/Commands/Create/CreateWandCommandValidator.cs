2821b0a0117d1c52aa57f90183001058b545b248
Maks0s <mrmaksman01@mail.ru>
2023-09-14 11:37:07 +0300

    Remove validation attributes from CreateWandRequest & add CreateWandCommandValidator

---
+++ Application/Wands/Commands/Create/CreateWandCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace Application.Wands.Commands.Create
+{
+    public class CreateWandCommandValidator 
+        : AbstractValidator<CreateWandCommand>
+    {
+        public CreateWandCommandValidator()
+        {
+            RuleFor(cwc => cwc.Core)
+                .NotEmpty()
+                .Length(3, 25);
+
+            RuleFor(cwc => cwc.Wood)
+                .NotEmpty()
+                .Length(3, 15);
+
+            RuleFor(cwc => cwc.LengthInInches)
+                .NotEmpty()
+                .Must(lengthInInches =>
+                    lengthInInches > 5 &&
+                    lengthInInches < 18);
+
+            RuleFor(cwc => cwc.Owner)
+                .Length(5, 80);
+
+            RuleFor(cwc => cwc.Description)
+                .NotEmpty()
+                .Length(30, 150);
+        }
+    }
+}
--- Presentation/Common/DTO/Request/CreateWandRequest.cs
+++ Presentation/Common/DTO/Request/CreateWandRequest.cs
@@ -3,30 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Presentation.Common.DTO.Request
 {
-    public class CreateWandRequest
-    {
-        [Required]
-        [MinLength(3)]
-        [MaxLength(25)]
-        public string Core { get; set; } = "";
-
-        [Required]
-        [MinLength(3)]
-        [MaxLength(15)]
-        public string Wood { get; set; } = "";
-
-        [Required]
-        [Range(6, 17)]
-        [Column(TypeName = "decimal(4,2)")]
-        public decimal LengthInInches { get; set; }
-
-        [MinLength(5)]
-        [MaxLength(80)]
-        public string? Owner { get; set; }
-
-        [Required]
-        [MinLength(30)]
-        [MaxLength(150)]
-        public string Description { get; set; } = "";
-    }
+    public record CreateWandRequest(
+        string Core,
+        string Wood,
+        decimal LengthInInches,
+        string? Owner,
+        string Description);
 }
